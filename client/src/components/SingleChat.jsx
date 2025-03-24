6ab80d1404f706452e9c7555693004af427d71fb
Ramvignesh27 <mgramvignesh@gmail.com>
2025-03-24 13:29:05 +0530

    changed url in singlechat

--- client/src/components/SingleChat.jsx
+++ client/src/components/SingleChat.jsx
@@ -16,7 +16,7 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
 import { axiosInstance } from "../api";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://chat-forever-client.vercel.app"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
